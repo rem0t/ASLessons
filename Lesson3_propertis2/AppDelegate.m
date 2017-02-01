132873c69d2987e76fce2a459da1016227f0a40a
Vladislav Kalaev <vladislavkalaev@MacBook-Air-Vladislav.local>
2017-02-01 18:29:35 +0300

    15.12.16

--- Lesson3_propertis2/AppDelegate.m
+++ Lesson3_propertis2/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     [self testObject];
-    
+     
     
     NSLog(@"TEST IS OVER");
     
